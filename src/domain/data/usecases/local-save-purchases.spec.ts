d2dabd2761436fe5b0d65052a1c77ed763a007c0
CELS0 <celsojr.300@gmail.com>
2022-02-13 18:13:00 -0300

    test: delete cache

---
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -0,0 +1,59 @@
+class LocalSavePurchases {
+  constructor(private cacheStore: CacheStore) { }
+
+  async save(key: string): Promise<void> {
+    this.cacheStore.delete(key);
+  }
+}
+
+
+interface CacheStore {
+  delete: (key: string) => void;
+}
+
+class CacheStoreSpy implements CacheStore {
+  deleteCallsCount = 0;
+  key: string;
+
+  delete(key: string): void {
+    this.deleteCallsCount++
+    this.key = key;
+  }
+}
+
+type SutTypes = {
+  sut: LocalSavePurchases;
+  cacheStore: CacheStoreSpy;
+}
+
+const makeSut = (): SutTypes => {
+  const cacheStore = new CacheStoreSpy();
+
+  const sut = new LocalSavePurchases(cacheStore)
+
+  return { cacheStore, sut }
+}
+
+describe('LocalSavePurchases', () => {
+  test('Should not delete cache on sut.init', () => {
+    const {  cacheStore } = makeSut();
+
+    expect(cacheStore.deleteCallsCount).toBe(0)
+  });
+
+  test('Should delete old cache on sut.save', async () => {
+    const { sut, cacheStore } = makeSut();
+
+    await sut.save('purchases');
+
+    expect(cacheStore.deleteCallsCount).toBe(1)
+  });
+
+  test('Should call delete with correct key', async () => {
+    const { sut, cacheStore } = makeSut();
+
+    await sut.save('purchases');
+
+    expect(cacheStore.key).toBe('purchases')
+  });
+});
